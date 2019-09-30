8e17149177b802af571da2d040c59a5124073d22
Denis Arsenault <denis2@me.com>
2019-09-29 21:14:43 -0400

    Initial Commit

---
+++ EmbeddedView/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  EmbeddedViews
+//
+//  Created by Denis Arsenault on 9/29/19.
+//  Copyright © 2019 mybrightzone. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate
+{
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
+    {
+        return true
+    }
+}
